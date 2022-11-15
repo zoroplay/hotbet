ababac6d2334e8785db3fa7b784fe825159f17f9
elsopi <elsopi02@yahoo.com>
2022-11-15 07:39:15 +0100

    work for selection

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,23 @@
-import axios from 'axios'
-import store from '../store'
+import axios from "axios";
+import store from "../store";
 
 const instance = axios.create({
-    baseURL: "https://sports-api.sportsbookengine.com/api",
-    timeout: 45000,
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-    },
+  baseURL: "https://sports-api.sportsbookengine.com/api",
+  timeout: 45000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  }
 });
 
-instance.interceptors.request.use((config) => {
-    const access_token = store.state.auth.access_token
-    if (access_token)
-      config.headers["Authorization"] = `Bearer ${access_token}`;
-  
-    return config;
-  });
-  
+instance.interceptors.request.use(config => {
+  const access_token = store.state.auth.access_token;
+  if (access_token) config.headers["Authorization"] = `Bearer ${access_token}`;
+
+  return config;
+});
 
 // // instance.defaults.withCredentials = true
 // instance.defaults.headers.post['Content-Type'] = 'application/json';
 // // instance.defaults.method = "post";
-export default instance
+export default instance;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import auth from './modules/auth';
+import auth from "./modules/auth";
+import coupon from "./modules/coupon";
 import createPersistedState from "vuex-persistedstate";
+import VuexPersistence from "vuex-persist";
+import axios from "../services/api";
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+});
 
 export default new Vuex.Store({
   state: {
@@ -11,29 +18,47 @@ export default new Vuex.Store({
     sport_id: 1,
     fixtures: [],
     live: false,
+    SportsbookBonusList: [],
+    SportsbookGlobalVariable: {}
   },
   mutations: {
-    setLive(state, value){
+    setLive(state, value) {
       state.live = value;
     },
-    setMenu(state, payload){
+    setMenu(state, payload) {
       state.sports = payload;
+    },
+    setBonusList(state, payload) {
+      state.SportsbookBonusList = payload;
+    },
+    setGlobalVars(state, payload) {
+      state.SportsbookGlobalVariable = payload;
     }
   },
   actions: {
-    setCommitLive({commit}, value){
-      commit('setLive', value)
+    setCommitLive({ commit }, value) {
+      commit("setLive", value);
     },
-    setCommitMenu({commit},value){
-      commit('setMenu', value)
+    setCommitMenu({ commit }, value) {
+      commit("setMenu", value);
+    },
+    setCommitSiteData({ commit }) {
+      Promise.all([fetchBonusList(), fetchGlobalVars()]).then(res => {
+        commit("setBonusList", res[0].data);
+        commit("setGlobalVars", res[1].data);
+      });
     }
   },
   modules: {
-    auth
+    auth,
+    coupon
   },
-  plugins: [createPersistedState()]
+  plugins: [createPersistedState(), vuexLocal.plugin]
 });
 
+const fetchGlobalVars = () => axios.get(`/utilities/globalvariables`);
+
+const fetchBonusList = () => axios.get(`/utilities/bonuslist?section=onliners`);
 
 // export const getOpenBets = () =>
 //     Http.get(`user/account/open-bets`);
@@ -52,7 +77,6 @@ export default new Vuex.Store({
 
 // export const redeemBonus = () =>
 //     Http.get(`user/account/redeem-bonus?source=mobile`);
-
 
 // export const getWithdrawalInfo = () =>
 //     Http.get(`user/account/withdrawal-info` );
